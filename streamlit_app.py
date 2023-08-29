ad9a9f861d916ec360c31b02595a1e3adf9585ad
sachinsandeep <33233491+sachinsandeep@users.noreply.github.com>
2023-08-28 22:11:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,6 @@ streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 
-streamlit.title('My Parents New Healthy Diner')
-
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 
 streamlit.text('Half-Boiled Free-Range Egg')
